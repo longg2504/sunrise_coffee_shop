d9a6e45fe25d651f0e084d0c9760707b88f593a6
longg2504 <longg2504@gmail.com>
2023-10-11 09:21:16 +0700

    upload

--- repository/src/main/java/com/cg/repository/supplierIngredient/SupplierIngredientRepository.java
+++ repository/src/main/java/com/cg/repository/supplierIngredient/SupplierIngredientRepository.java
@@ -2,6 +2,8 @@ package com.cg.repository.supplierIngredient;
 
 import com.cg.domain.entity.SupplierIngredient;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface SupplierIngredientRepository extends JpaRepository<SupplierIngredient,Long> {
 }
